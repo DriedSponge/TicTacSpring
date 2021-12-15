27e437339a135d2d2029e83b7a4c1d39ca5d0272
DriedSponge <jordan@driedsponge.net>
2021-12-15 09:31:33 -0800

    UI Updates

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
   import "../app.css";
 </script>
-
-  <main>
-    <section class="container mx-auto">
-      <slot />
-    </section>
-  </main>
-  <footer>
-    <p>
-      Made by Jordan
-    </p>
-  </footer>
+  <div class="flex flex-col min-h-screen">
+    <main class="flex-grow">
+      <section class="container mx-auto max-w-6xl">
+        <slot />
+      </section>
+    </main>
+     <footer class="py-5">
+      <p class="text-center font-bold italic">
+        Made by Jordan
+      </p>
+    </footer>
+  </div> 
