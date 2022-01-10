6a6c32244c4796aef3970d0d3a725e5885954bee
DriedSponge <jordan@driedsponge.net>
2022-01-10 00:28:54 -0800

    login

---
+++ frontend/src/lib/Login.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import axios from "axios";
+
+  export const prerender = true;
+  let name: string = "";
+  function login(e) {
+    console.log(name);
+    if (name.length > 0) {
+      console.log("valid name");
+    } else {
+      console.log("Invalid name");
+    }
+  }
+</script>
+
+<form on:submit|preventDefault={login} autocomplete="off">
+  <div class="flex flex-col">
+    <div class="w-full">
+      <input
+        name="name"
+        id="name"
+        maxlength="30"
+        bind:value={name}
+        placeholder="Your Name"
+      />
+    </div>
+    <div class="w-full">
+      <button type="submit">Go</button>
+    </div>
+  </div>
+</form>
+<style lang="postcss">
+    button {
+      @apply py-2 appearance-none rounded-b-lg border-t-0 rounded-t-none shadow-lg bg-blue-500 hover:bg-blue-600 text-white font-bold text-xl;
+      @apply transition ease-in-out duration-300 w-full;
+    }
+    input {
+      @apply p-2;
+      @apply appearance-none;
+      @apply focus:ring-0 focus:outline-none;
+      @apply transition duration-300 ease-in-out;
+      @apply w-full rounded-t-lg rounded-b-none border-gray-100 border-2 focus:border-blue-200;
+      @apply placeholder:text-xl placeholder:text-center;
+      @apply text-center font-bold text-xl
+    }
+  </style>
