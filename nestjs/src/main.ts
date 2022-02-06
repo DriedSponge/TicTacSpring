15cc65d6f365e07d724f59b7520b7c7b2605b899
DriedSponge <jordan@driedsponge.net>
2022-02-06 10:15:33 -0800

    better validation exceptions

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,16 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
-import { ValidationPipe } from '@nestjs/common';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { ValidationError } from 'class-validator';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
-    validationError:{target:false},
+    validationError:{target:false, value: false},
     forbidUnknownValues: true,
     skipUndefinedProperties:true,
     forbidNonWhitelisted: true,
     disableErrorMessages: false,
     whitelist: true,
+    exceptionFactory: (validationErrors: ValidationError[] = []) => {
+      return new BadRequestException(validationErrors);
+    }
   }));
   await app.listen(8080);
 }
