d3cfc8b2a4a45ab26cfeb2e08e259fc40c6a7e1b
DriedSponge <jordan@driedsponge.net>
2022-02-05 22:49:53 -0800

    Password encryption

--- nestjs/src/auth/auth.controller.ts
+++ nestjs/src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { Prisma, User } from '@prisma/client';
 import { Response } from 'express';
 import { CreateUserDto } from 'src/user/CreateUserDto';
 import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Controller('auth')
 export class AuthController {
@@ -13,6 +14,9 @@ export class AuthController {
 
     @Post("register")
     async register(@Body() userData: CreateUserDto) {
+        
+        const salt = await bcrypt.genSalt();
+        userData.password = await bcrypt.hash(userData.password, salt);
         return this.userService.createUser({data:userData});
     }
 }
