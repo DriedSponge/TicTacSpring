829b7ba0fcc92359751c4706c22fd30fc6252010
DriedSponge <jordan@driedsponge.net>
2022-03-14 23:04:16 -0700

    Stufff

--- nestjs/src/game/game.controller.ts
+++ nestjs/src/game/game.controller.ts
@@ -1,12 +1,16 @@
 import { BadRequestException, Body, Controller, Get, Session, HttpException, HttpStatus, Post, Req, Res, UseGuards, Request } from '@nestjs/common';
 import {SessionGuard} from "../auth/session.guard";
 import { GameService } from './game.service';
+import {Game } from '@prisma/client';
+
 @Controller('game')
 export class GameController {
     constructor(private readonly gameService: GameService){}
 @Post("create")
   @UseGuards(SessionGuard)
-  getSession(@Request() req, @Res() response) {
-    response.status(200).json({ success: true, gameId:1234 });
+  async getSession(@Request() req, @Res() response) {
+    const game: Game  = await this.gameService.createGame();
+    
+    response.status(200).json({ success: true, gameId:game.code });
   }
 }
--- nestjs/src/game/game.service.ts
+++ nestjs/src/game/game.service.ts
@@ -7,7 +7,30 @@ export class GameService {
     constructor(private prisma: PrismaService) {}
 
     async createGame(): Promise<Game>{
-        return this.prisma.game.create({data:{code:1111}});
+        let code:number = Math.floor(10000 + Math.random() * 90000);
+        let found: boolean = false;
+        while(!found){
+            try {
+                console.debug("Looking for code dupe...")
+                await this.getGameByCode(code)
+                console.debug("Dupe found, trying a new code...")
+                Math.floor(10000 + Math.random() * 90000)
+            }catch{
+                found = true
+                console.debug("Code dupe not found. Creating game instance!")
+                return this.prisma.game.create({data:{code:code}});
+            }
+        }
+        
+    }
+
+    async getGameByCode(code: number): Promise<Game>{
+        return this.prisma.game.findFirst({
+            where:{
+                code:code
+            },
+            rejectOnNotFound: true
+        })
     }
 
 }
